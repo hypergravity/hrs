dcbec629230b09bd6fd785415fb71d05448a4706
Bo Zhang <bozhang@nao.cas.cn>
2016-11-22 22:09:16 +0800

    add setup.py: setup for hrs

---
+++ README.md
@@ -0,0 +1,51 @@
+HRS
+===
+
+High Resolution Spectrograph (2.16m) Reduction pipeline.
+
+- Github url: [https://github.com/hypergravity/hrs](https://github.com/hypergravity/hrs)
+- PYPI url: [https://pypi.python.org/pypi/hrs](https://pypi.python.org/pypi/hrs)
+
+AUTHOR
+======
+
+- Bo Zhang (@NAOC, bozhang@nao.cas.cn)
+
+If you find this code useful in your research, please let me know. Thanks!
+
+Any people are welcome to contribute to this package.
+
+
+CONTRIBUTORS
+============
+
+- Bo Zhang (@NAOC, bozhang@nao.cas.cn)
+
+
+DOCUMENTATION
+=============
+
+Will be featured in the near future.
+
+
+INSTALL
+=======
+`sudo pip install -U bopy` or `pip install --user hrs`
+
+
+PYTHON VERSIONS AND DEPENDENCIES
+================================
+
+- python 2.7.12
+- ipython 4.0.0
+
+
+ISSUES
+======
+**HRS** is originally designed to work under python2.7, but will support python3.X in the future.
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='hrs',
+        version='0.0.0',
+        author='Bo Zhang',
+        author_email='bozhang@nao.cas.cn',
+        # py_modules=['hrs'],
+        description='High Resolution Spectrograph (2.16m) Reduction pipeline.',  # short description
+        license='New BSD',
+        # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
+        url='http://github.com/hypergravity/hrs',
+        classifiers=[
+            "Development Status :: 6 - Mature",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Scientific/Engineering :: Astronomy",
+            "Topic :: Scientific/Engineering :: Physics"],
+        package_dir={'hrs/': ''},
+        packages=['hrs',
+                  ],
+        package_data={'hrs/data': [''],
+                      "":          ["LICENSE"]},
+        include_package_data=True,
+        requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'lmfit']
+    )
