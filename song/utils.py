cbd60923a412a25189b790eb6ab73f810df63163
Bo Zhang <bozhang@nao.cas.cn>
2017-02-24 20:14:20 +0800

    update utils.py: add  utils to scan files

--- song/utils.py
+++ song/utils.py
@@ -11,7 +11,7 @@ bozhang@nao.cas.cn
 
 Created on
 ----------
-- Sun Jan  8 14:00:00 2016
+- Sun Jan  8 14:00:00 2017
 
 Modifications
 -------------
@@ -36,29 +36,53 @@ from joblib import Parallel, delayed
 
 
 # scan fits header
-def scan_fits_header(fps):
+def scan_fits_header(fps, verbose=True):
     cfn = sys._getframe().f_code.co_name
-    for i in trange(len(fps), ncols=100, ascii=False, desc="**" + cfn + "**",
-                    unit=" file"):
-        fp = fps[i]
-        #        print i, len(fps)
-        # read fits
-        hl = fits.open(fp)
-        hdr_dict = dict(hl[0].header)
 
-        # initiate table
-        if i == 0:
-            t = Table()
-            for k, v in hdr_dict.items():
-                #                print k, v
-                if type(v) is str:
-                    t.add_column(
-                        Column(data=np.array([]), dtype=object, name=k))
-                else:
-                    t.add_column(
-                        Column(data=np.array([]), dtype=type(v), name=k))
-        # add rows
-        t.add_row(hdr_dict.values())
+    if verbose:
+        # use tgdm.trange
+        for i in trange(len(fps), ncols=100, ascii=False,
+                        desc="**" + cfn + "**", unit=" file"):
+            fp = fps[i]
+
+            # read fits
+            hl = fits.open(fp)
+            hdr_dict = dict(hl[0].header)
+
+            # initiate table
+            if i == 0:
+                t = Table()
+                for k, v in hdr_dict.items():
+                    if type(v) is str:
+                        t.add_column(
+                            Column(data=np.array([]), dtype=object, name=k))
+                    else:
+                        t.add_column(
+                            Column(data=np.array([]), dtype=type(v), name=k))
+            # add row
+            t.add_row(hdr_dict.values())
+    else:
+        for i in range(len(fps)):
+            fp = fps[i]
+
+            # read fits
+            hl = fits.open(fp)
+            hdr_dict = dict(hl[0].header)
+
+            # initiate table
+            if i == 0:
+                t = Table()
+                for k, v in hdr_dict.items():
+                    if type(v) is str:
+                        t.add_column(
+                            Column(data=np.array([]), dtype=object, name=k))
+                    else:
+                        t.add_column(
+                            Column(data=np.array([]), dtype=type(v), name=k))
+            # add row
+            t.add_row(hdr_dict.values())
+
+    # add fps column
     t.add_column(Column(fps, "fps"), 0)
 
     # fix column dtype
@@ -71,11 +95,11 @@ def scan_fits_header(fps):
     return t
 
 
-def scan_files(dirpath, xdriftcol=True):
+def scan_files(dirpath, xdriftcol=True, verbose=True):
 
-    fps_all = glob.glob(dirpath+"/*.fits")
+    fps_all = glob.glob(dirpath + "/*.fits")
     fps_all.sort()
-    t = scan_fits_header(fps_all)
+    t = scan_fits_header(fps_all, verbose=verbose)
 
     if xdriftcol:
         t.add_column(Column(np.zeros((len(t),), dtype=int) * np.nan, "xdrift"))
@@ -85,7 +109,7 @@ def scan_files(dirpath, xdriftcol=True):
 
 
 def scan_flux90(t, ind_scan=None, ind_type='bool', pct=90,
-                n_jobs=-1, verbose=10, unit='adu', **kwargs):
+                n_jobs=-1, verbose=10, **kwargs):
     """
 
     Parameters
